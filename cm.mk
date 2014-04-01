aed10544f255c417f8c509ff29d8db744ece9c39
Matt Mower <mowerm@gmail.com>
2014-04-01 16:53:31 -0500

    Override product name
    Change-Id: Id4e6a4be2ef098a18481d0d76d8d06b7de34b8b0

--- cm.mk
+++ cm.mk
@@ -5,11 +5,14 @@ PRODUCT_RELEASE_NAME := ville
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/htc/ville/device_ville.mk)
+$(call inherit-product, device/htc/ville/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ville
 PRODUCT_NAME := cm_ville
 PRODUCT_BRAND := htc
-PRODUCT_MODEL := ville
-PRODUCT_MANUFACTURER := htc
+PRODUCT_MODEL := One S
+PRODUCT_MANUFACTURER := HTC
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=ville
