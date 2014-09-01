90829404417e9197748d3935e2035e89b66b701d
Matt Mower <mowerm@gmail.com>
2014-08-31 20:22:01 -0500

    Use BatteryLED library for charging indicator
    Change-Id: I8981249fed55ab5fa1e95bc19ab2bc9ab8c73870

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -103,6 +103,6 @@ TW_INCLUDE_DUMLOCK := true
 TW_INCLUDE_JB_CRYPTO := true
 TW_NO_SCREEN_BLANK := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
-TW_HTC_LED := true
+TW_BATTERY_LED := htc-legacy
 BOARD_RECOVERY_BLDRMSG_OFFSET := 2048
 RECOVERY_VARIANT := twrp
