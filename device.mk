798976a46c7e5f75f8dd321c21c67e407a993a41
Matt Mower <mowerm@gmail.com>
2014-09-12 21:34:19 -0500

    Add chargeled program for recovery
    A small program to update the LED indicator based on charging status in
    recovery. Build-blocked on RECOVERY_VARIANT=twrp so it doesn't interfere
    with msm8960-common.
    * Green LED for charged
    * Amber LED for charging
    * LED off all other cases
    
    Change-Id: I063013e312f5783f135732f63f8b4e1ba3695915

--- device.mk
+++ device.mk
@@ -25,6 +25,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/detect_key:recovery/root/sbin/detect_key \
     $(LOCAL_PATH)/recovery/fstab.qcom:recovery/root/fstab.qcom
 
+PRODUCT_PACKAGES += \
+    chargeled
+
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
