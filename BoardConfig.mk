c92f276fb15a3769d8480214483b4d9f24ad17a4
Matt Mower <mowerm@gmail.com>
2015-01-30 13:32:22 -0600

    Update configuration for cm-12.0
    Change-Id: Idb00d99dd296f31e451917247306c63247ac4be6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,3 +1,19 @@
+# Copyright (C) 2015 Matthew D. Mower
+# Copyright (C) 2015 The CyanogenMod Project
+# Copyright (C) 2013 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 USE_CAMERA_STUB := true
 
 # inherit from the proprietary version
@@ -13,27 +29,23 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
 
 # Architecture
 TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
 TARGET_CPU_VARIANT := krait
-TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80400000
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 2048
-TARGET_KERNEL_CONFIG := valente_wx_defconfig
-TARGET_KERNEL_SOURCE := kernel/htc/valentewx
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01700000
+TARGET_KERNEL_CONFIG := valente_wx_defconfig
+TARGET_KERNEL_SOURCE := kernel/htc/msm8960
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
-TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
-TARGET_QCOM_AUDIO_VARIANT := caf
-TARGET_QCOM_DISPLAY_VARIANT := caf
-TARGET_QCOM_MEDIA_VARIANT := caf
-TARGET_USES_QCOM_BSP := true
 
 # Flags
 COMMON_GLOBAL_CFLAGS += -DHTCLOG
@@ -44,11 +56,12 @@ TARGET_DISPLAY_USE_RETIRE_FENCE := true
 TARGET_USES_C2D_COMPOSITION := true
 TARGET_USES_ION := true
 BOARD_EGL_CFG := device/htc/valentewx/configs/egl.cfg
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 
 # Power
-TARGET_POWERHAL_VARIANT := cm
+TARGET_POWERHAL_VARIANT := qcom
 
-# Filesystem (cat /proc/emmc)
+# Filesystem
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16776704
@@ -59,11 +72,11 @@ BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16776704
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 13153337344
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-# Use power button as select in recovery
+# Recovery
 BOARD_HAS_NO_SELECT_BUTTON := true
 
 # Vold
-BOARD_VOLD_MAX_PARTITIONS := 36
+BOARD_VOLD_MAX_PARTITIONS := 37
 # lun files are located at /sys/devices/platform/msm_hsusb/gadget/lun%d/file
 # but we need to add/remove symlinks to them based on whether there are one
 # or two drives available to UMS. This is handled in init.
@@ -73,40 +86,25 @@ TARGET_USE_CUSTOM_LUN_FILE_PATH := /lun%d/file
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/htc_lpm/lpm_mode
 
 # SELinux
+-include device/qcom/sepolicy/sepolicy.mk
+
 BOARD_SEPOLICY_DIRS += device/htc/valentewx/sepolicy
 
 BOARD_SEPOLICY_UNION += \
-    app.te \
-    bluetooth.te \
-    device.te \
-    domain.te \
-    drmserver.te \
-    file_contexts \
-    file.te \
-    hci_init.te \
-    healthd.te \
-    init_shell.te \
-    init.te \
-    keystore.te \
-    kickstart.te \
-    mediaserver.te \
-    rild.te \
-    surfaceflinger.te \
-    system.te \
-    ueventd.te \
-    wpa_socket.te \
-    wpa.te
+    akmd.te \
+    ewtzmud.te
 
 # TWRP
 DEVICE_RESOLUTION := 540x960
-TW_FLASH_FROM_STORAGE := true
-BOARD_HAS_NO_REAL_SDCARD := true
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-TW_INCLUDE_DUMLOCK := true
-TW_INCLUDE_JB_CRYPTO := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TW_NO_SCREEN_BLANK := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_INCLUDE_CRYPTO := true
 BOARD_RECOVERY_BLDRMSG_OFFSET := 2048
 RECOVERY_VARIANT := twrp
 TARGET_RECOVERY_DEVICE_MODULES := chargeled
+BOARD_HAS_NO_REAL_SDCARD := true
+TW_INCLUDE_DUMLOCK := true
+TW_EXTERNAL_STORAGE_PATH := "/usb-otg"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "usb-otg"
